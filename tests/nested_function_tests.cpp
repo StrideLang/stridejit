e2d22c326341f1b329d375946f2cbf853dc41c4f
Andres Cabrera <mantaraya36@gmail.com>
2026-09-17 13:54:43 -0700

    Add unit test for LoopInReaction permutation

--- tests/nested_function_tests.cpp
+++ tests/nested_function_tests.cpp
@@ -145,3 +145,31 @@ TEST(NestedFunctions, ReactionInReaction) {
   Entry(&in2, &out2);
   EXPECT_DOUBLE_EQ(out2, 12.5);
 }
+
+TEST(NestedFunctions, LoopInReaction) {
+  strd::StrideEnvironment strenv;
+
+  auto ret =
+      strenv.generateIr(STRIDEJIT_TESTS_SOURCE_DIR "loop_in_reaction.stride");
+  ASSERT_TRUE(ret);
+
+  ret = strenv.compileInMemory();
+  ASSERT_TRUE(ret);
+
+  llvm::Expected<llvm::orc::ExecutorAddr> EntrySym =
+      strenv.getFunction("RootDomain_process");
+  ASSERT_TRUE(static_cast<bool>(EntrySym));
+
+  auto *Entry = EntrySym->toPtr<void (*)(int32_t *, int32_t *)>();
+  ASSERT_NE(Entry, nullptr);
+
+  int32_t in1 = 1;
+  int32_t out1 = 0;
+  Entry(&in1, &out1);
+  EXPECT_EQ(out1, 3);
+
+  int32_t in2 = 10;
+  int32_t out2 = 0;
+  Entry(&in2, &out2);
+  EXPECT_EQ(out2, 12);
+}
